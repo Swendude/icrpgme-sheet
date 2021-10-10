ef915639c910a1b5a5e5d2e99dc0f2dcaa2ca979
Swendude <swenmulderij@gmail.com>
2021-10-10 20:43:45 +0200

    Added tailwind (and 1000 other things).

--- src/features/character/Character.tsx
+++ src/features/character/Character.tsx
@@ -1,25 +1,44 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 
 import { useAppSelector, useAppDispatch } from "../../app/hooks";
-import {
-  decrement,
-  increment,  
-  selectCharacter
-} from "./characterSlice";
+import { decrement, increment, selectCharacter } from "./characterSlice";
 
-import styles from './character.module.css'
+type TextPropProps = {
+  label: string;
+  value: string;
+};
+
+function TextProp(props: TextPropProps) {
+  const [editing, setEditing] = useState(false);
+
+  return (
+    <div className="pl-2 border-2 border-black">
+      <p className="text-xs text-black text-opacity-40">{props.label}</p>
+      {!editing ? (
+        <div onClick={() => setEditing(!editing)}>
+          <p>{props.value || `Please enter ${props.label}`}</p>
+        </div>
+      ) : (
+        <input
+          value={props.value || `Please enter ${props.label}`}
+          type="text"
+        ></input>
+      )}
+    </div>
+  );
+}
 
 export function Character() {
   const character = useAppSelector(selectCharacter);
   const dispatch = useAppDispatch();
-  
+
   return (
-    <div className={styles.box_style}>
-      <div>
-        <div>
-          <p>Name:{character.name || "NO NAME"}</p>
-        </div>
-      </div>
+    <div className=" m-4 max-w-sm mx-auto bg-white rounded shadow-md border-4 border-black">
+      <TextProp label="Name" value={character.name} />
+      <TextProp label="Type" value={character.type} />
+      <TextProp label="Lifeform" value={character.lifeform} />
+      <TextProp label="Story" value={character.story} />
+
       <div>
         <div>
           <p>Coin:</p>
